4c2fa09e52686f5f952df27ca790430f60cef6f3
devana <devana728@rambler.ru>
2023-09-12 02:04:49 +0300

    first commit

---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+      <p>name {{ product.name }}</p>
+      <p> price {{ formattedPrice }}</p>
+      <p v-if="product.available">Available</p>
+      <p v-else>Out of stock</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+   name: 'ProductDetails',
+    
+   data() {
+    return {
+      product: 
+        { 
+            name: 'Product-1', 
+            price: 99.99,
+            available: true, 
+        },
+    };
+  },
+  computed: {
+    formattedPrice: function() {
+        return`$${this.product.price}`;
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
